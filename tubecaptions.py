eaf20f8a736a0b1dc139b96ca83417f3b7e437a4
Neil Barduson <barduson.neil@gmail.com>
2017-03-29 04:03:46 -0400

    init
    instantiating the first tube captions python script

---
+++ tubecaptions.py
@@ -0,0 +1,20 @@
+import pysrt
+from subprocess import call
+import subprocess as sp
+import os
+
+def getCapText(vidId, fileName):
+	command = "youtube-dl --write-auto-sub --sub-lang en --skip-download -o "+fileName+".%(ext)s http://www.youtube.com/watch?v="+vidId
+	call(command.split(), shell=False)
+
+	cmd = "ffmpeg -i "+fileName+".en.vtt "+fileName+".en.srt"
+	call(cmd.split(), shell=False)
+
+	subs = pysrt.open(fileName+'.en.srt')
+	with open(fileName+".txt", "w") as text_file:
+		os.remove(fileName+".en.vtt")
+		os.remove(fileName+".en.srt")
+		text_file.write(subs.text)
+		print 'created '+fileName+".txt!"
+
+getCapText('wxiV4MFhJh8','progamming')
